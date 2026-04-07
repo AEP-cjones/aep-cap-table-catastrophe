4587d1fb4b09f9a2e6dd9f0d7ff75bcdd9449cd1
AEP-cjones <carson.jones01@outlook.com>
2026-04-07 15:51:39 -0600

    Connect Firebase: cap-table-catastrophe project

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,15 +1,15 @@
 import { initializeApp } from 'firebase/app'
 import { getDatabase } from 'firebase/database'
 
-// TODO: Replace with real Firebase config before deployment
 const firebaseConfig = {
-  apiKey: "PLACEHOLDER_API_KEY",
-  authDomain: "PLACEHOLDER_PROJECT.firebaseapp.com",
-  databaseURL: "https://PLACEHOLDER_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "PLACEHOLDER_PROJECT",
-  storageBucket: "PLACEHOLDER_PROJECT.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:0000000000000000000000"
+  apiKey: "AIzaSyAHnvT7oyR9HclZmcO-5ehkR1kMeVmzkSg",
+  authDomain: "cap-table-catastrophe.firebaseapp.com",
+  databaseURL: "https://cap-table-catastrophe-default-rtdb.firebaseio.com",
+  projectId: "cap-table-catastrophe",
+  storageBucket: "cap-table-catastrophe.firebasestorage.app",
+  messagingSenderId: "777636045302",
+  appId: "1:777636045302:web:b594763cbe0d208de527d7",
+  measurementId: "G-GDSQ4WS9K4"
 }
 
 const app = initializeApp(firebaseConfig)
